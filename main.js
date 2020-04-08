1fe4d2494bedb55a9ead4b59556286f8d643b26d
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-08 15:09:59 +0200

    JS jQuery
    Esercizio completato

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ console.log('JS è stato inserito correttamente');
  * 
 \**************************************************/
 
+
+var hamburger = $('.header-right a');     // Selezionato l'elemento hamburger
+var cross = $('.close');                  // Selezionato l'elemento cross
+var hamburgerMenù = $('.hamburger-menu'); // Seleziona l'elemento hambur
+
+// Azione di apertura
+
+hamburger.click( function() {
+    //hamburgerMenù.show();         // Comparsa menù istantanea
+    hamburgerMenù.fadeIn();         // Comparsa menù lenta
+} );
+
+cross.click( function() {
+    //hamburgerMenù.hide();
+    hamburgerMenù.fadeOut();
+} );
+
