c3ce69814a6db4ea71f6dc3a1f3d209f05de22ea
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-08 14:33:43 +0200

    JS jQuery
    Inizio

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('JS è stato inserito correttamente');
+
+/***************************************************\
+ * 
+ * Rendere visibile in apertura e chiusura il menù.
+ * Non modificare HTML
+ * Non modificare CSS
+ * 
+\**************************************************/
+
